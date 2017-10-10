ddf30a61e00f33684e99694db20573dfd8dbfdeb
kriollo <kriollone@gmail.com>
2017-10-10 09:24:03 -0300

    Base de datos

--- ocrend.sql
+++ ocrend.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-10-2017 a las 20:09:44
+-- Tiempo de generación: 10-10-2017 a las 14:22:58
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 7.1.7
 
@@ -23,6 +23,39 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `ocrend` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `ocrend`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa`
+--
+
+DROP TABLE IF EXISTS `empresa`;
+CREATE TABLE `empresa` (
+  `id_empresa` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `impuesto` double NOT NULL,
+  `tipo_moneda` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `direccion` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `ciudad` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `comuna` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `region` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `ext_logo` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Truncar tablas antes de insertar `empresa`
+--
+
+TRUNCATE TABLE `empresa`;
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`id_empresa`, `nombre`, `telefono`, `email`, `impuesto`, `tipo_moneda`, `direccion`, `ciudad`, `comuna`, `region`, `ext_logo`) VALUES
+(1, 'WYS LTDA', '+56 9', 'contacto@wys.cl', 19, '$', 'Por Ahi', 'Santiago', 'Santiago', 'Metropolitana', 'jpg');
 
 -- --------------------------------------------------------
 
@@ -80,9 +113,12 @@ TRUNCATE TABLE `tblperfiles`;
 --
 
 INSERT INTO `tblperfiles` (`id`, `nombre`, `id_menu`, `id_submenu`) VALUES
-(39, 'HD_USUARIO', 1, 1),
-(41, 'HD_SUPERVISOR', 2, 1),
-(40, 'HD_SUPERVISOR', 1, 1);
+(78, 'HD_SUPERVISOR', 5, 1),
+(77, 'HD_SUPERVISOR', 4, 1),
+(79, 'HD_USUARIO', 1, 1),
+(76, 'HD_SUPERVISOR', 3, 1),
+(75, 'HD_SUPERVISOR', 2, 1),
+(74, 'HD_SUPERVISOR', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -114,8 +150,12 @@ INSERT INTO `tblperfilesuser` (`id`, `id_user`, `id_menu`, `id_submenu`) VALUES
 (1, '13', 1, 1),
 (1, '2', 1, 1),
 (1, '2', 2, 1),
+(1, '2', 3, 1),
+(1, '2', 4, 1),
+(1, '2', 5, 1),
 (1, '3', 1, 1),
 (1, '4', 1, 1),
+(1, '4', 2, 1),
 (1, '5', 1, 1),
 (1, '6', 1, 1),
 (1, '7', 1, 1),
@@ -148,13 +188,15 @@ TRUNCATE TABLE `tblsubmenu`;
 --
 
 INSERT INTO `tblsubmenu` (`id_menu`, `id_submenu`, `PosS`, `url`, `descripcion`, `estado`) VALUES
-(99, 1, 1, 'administracion/usuario', 'Usuarios', 1),
-(99, 2, 2, 'administracion/perfiles', 'Perfiles', 1),
+(99, 2, 2, 'administracion/usuario', 'Usuarios', 1),
+(99, 3, 3, 'administracion/perfiles', 'Perfiles', 1),
 (2, 1, 1, 'rrhh', 'Principal', 1),
 (1, 1, 1, 'plataforma', 'Principal', 1),
 (3, 1, 1, 'redes', 'Principal', 1),
 (5, 1, 1, 'prevencion', 'Principal', 1),
-(4, 1, 1, 'operaciones', 'Principal', 1);
+(4, 1, 1, 'operaciones', 'Principal', 1),
+(99, 1, 1, 'administracion', 'Principal', 1),
+(99, 4, 4, 'administracion/empresa', 'Datos Empresa', 1);
 
 -- --------------------------------------------------------
 
@@ -190,12 +232,12 @@ TRUNCATE TABLE `users`;
 --
 
 INSERT INTO `users` (`id_user`, `name`, `email`, `fono`, `cargo`, `pass`, `tmp_pass`, `token`, `perfil`, `rol`, `estado`, `foto`, `name_foto`, `pagina_inicio`) VALUES
-(1, 'ADMINISTRADOR DE SISTEMA', 'admin@wys.cl', '+56 555CORRIENTEE', 'ADMINISTRADOR SISTEMA', '$2a$10$9af81aa5eb4b6c474ce28OszU/gzJ0m228cqmbBHK5YOb6lh1LVLC', '', '', 'DEFINIDO', 1, 1, 1, '1.jpg', 'administracion/usuario'),
-(2, 'JORGE JARA', 'jjara@wys.cl', '+56988397298', 'Supervisor HD', '$2a$10$e1bb281b478e8fd0c335cOshRICjNlTqb1McoFLaScnITXbnoJOC.', '', '', 'HD_SUPERVISOR', 0, 1, 1, '2.jpg', 'plataforma'),
+(1, 'ADMINISTRADOR DE SISTEMA', 'admin@wys.cl', '+56 555CORRIENTEE', 'ADMINISTRADOR SISTEMA', '$2a$10$9af81aa5eb4b6c474ce28OszU/gzJ0m228cqmbBHK5YOb6lh1LVLC', '', '', 'DEFINIDO', 1, 1, 1, '1.jpg', 'administracion'),
+(2, 'JORGE JARA', 'jjara@wys.cl', '+56988397298', 'Supervisor HD', '$2a$10$e1bb281b478e8fd0c335cOshRICjNlTqb1McoFLaScnITXbnoJOC.', '', '', 'HD_SUPERVISOR', 0, 0, 1, '2.jpg', 'plataforma'),
 (3, 'PRUEBA DE EÃ‘E Y ÃCÃ‰NTÃ“Ã“Ã“Ã“Ã“Ã“Ã“', 'prueba@wys.cl', '111111111', 'otro', '$2a$10$687c1d9982b3f6742eb71u.p.anfVdE3biHUIMf3wADqg7dl9.ZRi', '', '', 'HD_USUARIO', 0, 1, 1, '3.jpg', 'portal'),
 (4, 'OTRO1', 'otro@wys.cl', '1', '1', '$2a$10$56623cd296fe02cf71a27uYr0W1bu/jPI8SWLdrx1RHm1Mj1h4dyC', '', '', 'DEFINIDO', 0, 1, 0, '', 'portal'),
 (5, 'OTRO2', 'otro2@otro.cl', '1', '1', '$2a$10$c9430121755f8051615c7eTGbhbxhOCHtx/qbP7d2oAGmBsz2Kznq', '', '', 'DEFINIDO', 0, 1, 0, '', 'portal'),
-(6, 'otro3', 'otro3@otro.cl', '', '', '$2a$10$adb8bf12568c963c73c19u1K0nJclJg5MqnI1mtgEajTMwulfcclK', '', '', 'DEFINIDO', 0, 0, 0, '', ''),
+(6, 'otro3', 'otro3@otro.cl', '', '', '$2a$10$adb8bf12568c963c73c19u1K0nJclJg5MqnI1mtgEajTMwulfcclK', '', '', 'DEFINIDO', 0, 1, 0, '', ''),
 (7, 'otro4', 'otro4@otro.cl', '', '', '$2a$10$8b74198346f00bd964ec7O2.Wh5yYtiNrW8fFq2D.aWZmeaMPhaXO', '', '', 'DEFINIDO', 0, 1, 0, '', ''),
 (8, 'otro5', 'otro5@otro.cl', '', '', '$2a$10$d29ae9464f94d617c16b4uIvhqUPzkazsTwVuN9NEKsIgpCcyddl6', '', '', 'DEFINIDO', 0, 1, 0, '', ''),
 (9, 'otro6', 'otro6@otro.cl', '', '', '$2a$10$ec73a76206c8c0f4ca1faOEiMkqF5njOePKvLAOFbQRhngaTDqitG', '', '', 'DEFINIDO', 0, 1, 0, '', ''),
@@ -207,6 +249,12 @@ INSERT INTO `users` (`id_user`, `name`, `email`, `fono`, `cargo`, `pass`, `tmp_p
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`id_empresa`);
 
 --
 -- Indices de la tabla `tblmenu`
@@ -243,6 +291,11 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT de la tabla `tblmenu`
 --
 ALTER TABLE `tblmenu`
@@ -251,7 +304,7 @@ ALTER TABLE `tblmenu`
 -- AUTO_INCREMENT de la tabla `tblperfiles`
 --
 ALTER TABLE `tblperfiles`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 --
 -- AUTO_INCREMENT de la tabla `tblperfilesuser`
 --
